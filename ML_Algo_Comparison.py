93d37599d81bb47e3accd843b1f19d7fa2761732
Ahmed Tariq <ahmed.tm097@gmail.com>
2022-02-26 13:57:37 +0500

    Logistic Regression

--- ML_Algo_Comparison.py
+++ ML_Algo_Comparison.py
@@ -1,34 +1,16 @@
 import numpy as np
 import pandas as pd
 
-dataset = pd.read_csv('Dataset/Crimes_2001_to_Present.csv')
-# X = dataset.iloc[:, :-14].values
+dataset = pd.read_csv('Dataset/Sample.csv')
 X = dataset.iloc[:, 1:22].values
+y = dataset.iloc[:, :-14].values
 
 print('Displaying DataSets for X \n', X)
-# print('\n Displaying DataSets for y \n', y)
+print('\n Displaying DataSets for y \n', y)
 
 # dealing with the missing values
 # Imputer class has been deprecated in newer versions so better use SimpleImpute
 from sklearn.impute import SimpleImputer
-
-# imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-# # axis 0 means mean of column, axi =1 means mean of row
-# # strategy mean, median, most_frequent
-# imputer = imputer.fit(X[:, 1:22])
-# X[:, 1:22] = imputer.transform(X[:, 1:22])
-# print('Filling the value using Mean method: \n')
-# print('Displaying DataSets for X \n', X)
-# # print('\n Displaying DataSets for y \n', y)
-
-# imputer = SimpleImputer(missing_values=np.nan, strategy='median')
-# imputer = imputer.fit(X[:, 1:22])
-# print('\n Splitting the Values In X \n')
-# print('Displaying DataSets for X \n', X)
-# X[:, 1:22] = imputer.transform(X[:, 1:22])
-# print('Filling the value using Median method: \n')
-# print('Displaying DataSets for X \n', X)
-# # print('\n Displaying DataSets for y \n', y)
 
 imputer = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
 imputer = imputer.fit(X[:, 1:22])
@@ -37,21 +19,33 @@ print(' Displaying DataSets for X \n', X)
 X[:, 1:22] = imputer.transform(X[:, 1:22])
 print('Filling the value using Most Frequent method: \n')
 print(' Displaying DataSets for X \n', X)
-# print('\n Displaying DataSets for y \n', y)
+print('\n Displaying DataSets for y \n', y)
 
 # Encoding categorical data
 
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 labelEncoder_X = LabelEncoder()
 X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
 print('\n Displaying DataSets in X after Encoding the first column \n', X)
 
 # Thus, we should also use OneHotEncoding by adding dummy columns as per number of distinct values in column country
-# onehotencoder = OneHotEncoder()
-# X = onehotencoder.fit_transform(X).toarray()
-# labelEncoder_y = LabelEncoder()
-# y = labelEncoder_y.fit_transform(y)
+onehotencoder = OneHotEncoder()
+X = onehotencoder.fit_transform(X).toarray()
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
 print('\n DataSets After Encoding and Sorting the column \n')
 print('Displaying DataSets for X \n', X)
-# print('\n Displaying DataSets for y \n', y)
+print('\n Displaying DataSets for y \n', y)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+print(X_train)
+
+from sklearn.linear_model import LogisticRegression
+
+lr = LogisticRegression(solver='liblinear',multi_class='ovr')
+lr.fit(X_train, y_train)
+print('Accuracy of Logistic Regression', lr.score(X_test, y_test))
