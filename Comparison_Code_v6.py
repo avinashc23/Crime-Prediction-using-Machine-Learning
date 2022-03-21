3ba1ab6cd50c8f336cd976efdee96c359830c033
Ahmed Tariq <78534449+ahmedtariq01@users.noreply.github.com>
2022-03-21 19:37:16 +0500

    Create Comparison_Code_v6.py

---
+++ Comparison_Code_v6.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import math
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.stats as stats
+from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
