1b5a663da86631544ffe78b39b52dc662352effe
Ahmed Tariq <78534449+ahmedtariq01@users.noreply.github.com>
2022-03-21 19:38:17 +0500

    Update Comparison_Code_v6.py

--- Comparison_Code_v6.py
+++ Comparison_Code_v6.py
@@ -8,3 +8,13 @@ from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
+
+dataset = pd.read_csv('Crimes_2001_to_Present (1).csv',low_memory=False)
+dataset.head(10)
+# dataset.dtypes
+
+# droping the features that are not usefull
+dataset=dataset.drop(columns=['ID','Case Number','Description','Updated On','Block'])
+# X.shape
+print('Columns in dataset: ', dataset.columns)
+dataset.shape
